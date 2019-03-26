f054d8cf094875a507dfd14ba9b5222bd20e9f91
Alexander Lee <alex.lee@zymoent.com>
2019-03-26 07:54:10 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+let budgetController = (function(){
+  let x = 23;
+
+  let add = function(a){
+    return x + a;
+  }
+
+  return {
+    publicTest: function(b){
+      return add(b);
+    }
+  }
+})();
+
+let UIController = (function(){
+  // some code
+})();
+
+let controller = (function(budgetCtrl, UICtrl){
+  let z = budgetCtrl.publicTest(9);
+  
+  return{
+    anotherPublic: function(){
+      console.log(z);
+    }
+  }
+})(budgetController, UIController);
+
